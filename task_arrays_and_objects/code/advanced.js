9d6eb08313eeb82f06e1bc679721ead747141e56
Faiz Fazaluddin <faiz.mf07@gmail.com>
2023-11-20 19:29:06 +0000

    completed advanced

--- task_arrays_and_objects/code/advanced.js
+++ task_arrays_and_objects/code/advanced.js
@@ -32,26 +32,35 @@ unitedKingdom = [
  * Q1. Add some tourist attractions to England
  */
 
-england = null;
-
-// console.log(england);
+england = unitedKingdom.find((country)=>country.name === "England");
+england.touristAttractions=["Gloucester Docks", "Tower of London"];
+console.log(england);
 
 /**
  * Q2. Change the capital of Wales to "Cardiff"
  */
 
-wales = null;
-
-// console.log(wales);
+wales = unitedKingdom.find((country)=>country.name==="Wales");
+wales.capital="Cardiff";
+console.log(wales);
 
 /**
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
 
-northernIrelandKeys = null;
-
-// console.log(northernIrelandKeys);
+northernIrelandKeys = Object.keys(unitedKingdom.find((country)=>country.name==="Northern Ireland"));
+console.log(northernIrelandKeys);
 
 /**
  * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, smallest or somewhere in the middle.
  */
+unitedKingdom.sort((a,b)=>b.population-a.population);
+
+console.log(unitedKingdom);
+if (unitedKingdom[0].name === "Scotland"){
+  console.log("biggest");
+} else if (unitedKingdom[unitedKingdom.length-1].name === "Scotland"){
+  console.log("smallest");
+} else {
+  console.log("In the middle");
+}
